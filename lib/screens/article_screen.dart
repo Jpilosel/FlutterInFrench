611288549fd30a93574070b855a01a6aedfb85c0
Jpilosel <joel@pilosel.fr>
2020-12-28 11:49:06 +0100

    Calcul auto de la hauteur du titre ArticleScreen

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -8,12 +8,13 @@ class ArcticleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _calcTitleHeight = MediaQuery.of(context).size.height * 0.15;
     return Scaffold(
       appBar: AppbarComponent.build(),
       body: ListView(
         children: <Widget>[
           Container(
-            height: 100.0,
+            height: _calcTitleHeight,
             child: FittedBox(
                 fit: BoxFit.fill,
                 child: Text(
