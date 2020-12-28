a8416eb054ba6e6355660a69d02b34b46203a0ef
Jpilosel <joel@pilosel.fr>
2020-12-28 09:28:29 +0100

    intégation controller+champ dynamique

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,31 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutterinfrench/models/article_model.dart';
+import 'package:provider/provider.dart';
 
+import '../controllers/article_controller.dart';
 import 'package:flutterinfrench/screens/components/appbar_component.dart';
 import 'package:flutterinfrench/screens/components/menu_component.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _controller = Provider.of<ArticleController>(context);
     return Scaffold(
       appBar: AppbarComponent.build(),
       drawer: MenuComponent(),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 25.0,
-          right: 10.0,
-          left: 10.0,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(child: Text('01-12-2020 15:50')),
-            Container(child: Text('Titre')),
-            Container(
-              child: Image.asset('assets/logoFlutter.png'),
-            )
-          ],
-        ),
+      body: FutureBuilder(
+        future: _controller.getArticles(),
+        builder: (context, AsyncSnapshot<List<ArticleModel>> snapshot) {
+          if (!snapshot.hasData) {
+            Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.data.first == null) {
+            return Container(
+              alignment: Alignment.center,
+              child: Text("Il n'y a pas d'article pour le moment."),
+            );
+          }
+          print(snapshot.data[0].imageUrl);
+          return Padding(
+            padding: const EdgeInsets.only(
+              top: 25.0,
+              right: 10.0,
+              left: 10.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(child: Text(snapshot.data[0].date)),
+                Container(child: Text(snapshot.data[0].description)),
+                Container(child: Text(snapshot.data[0].title)),
+                Container(
+                  child: Image.network(snapshot.data[0].imageUrl),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
