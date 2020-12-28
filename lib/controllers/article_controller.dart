3ab571c0ca92ef0ba70de170f8c01ac83d4bb185
Jpilosel <joel@pilosel.fr>
2020-12-28 08:26:53 +0100

    resolution problème gradle et champ vdie

--- lib/controllers/article_controller.dart
+++ lib/controllers/article_controller.dart
@@ -1,13 +1,42 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterinfrench/models/article_model.dart';
 
 class ArticleController extends ChangeNotifier {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  ArticleController() {
+    getArticles();
+  }
+
   Future getArticles() async {
-    QuerySnapshot dataFirestore = await _firestore
+    QuerySnapshot _dataFirestore = await _firestore
         .collection("articles")
         .orderBy("date", descending: true)
         .get();
+
+    List<ArticleModel> _articleData = _dataFirestore.docs.map((article) {
+      final _temp = ArticleModel(
+          title: article.get("name"),
+          description: article.get("description"),
+          date: article.get("date"),
+          imageUrl: article.get("preview"));
+      if (_temp.date == null) {
+        print("c'est null");
+      }
+      if (_temp.date != null &&
+          _temp.title != null &&
+          _temp.description != null &&
+          _temp.imageUrl != null) {
+        if (_temp.date.isNotEmpty &&
+            _temp.description.isNotEmpty &&
+            _temp.imageUrl.isNotEmpty &&
+            _temp.title.isNotEmpty) {
+          return _temp;
+        }
+      }
+    }).toList();
+    print(_articleData[0].date);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterinfrench/controllers/article_controller.dart';
+import 'package:provider/provider.dart';
+
 import 'package:flutterinfrench/screens/article_screen.dart';
 import 'package:flutterinfrench/screens/contact_screen.dart';
 import 'package:flutterinfrench/screens/home_screen.dart';
@@ -10,15 +13,19 @@ class FlutterInFrenchApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     precacheImage(AssetImage('assets/logoFlutter.png'), context);
-    return MaterialApp(
-      title: "Flutter in French",
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/contact': (context) => ContactScreen(),
-        'article': (context) => ArcticleScreen()
-      },
-      theme: MainTheme.build(),
+    return ChangeNotifierProvider(
+      lazy: false,
+      create: (context) => ArticleController(),
+      child: MaterialApp(
+        title: "Flutter in French",
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeScreen(),
+          '/contact': (context) => ContactScreen(),
+          'article': (context) => ArcticleScreen()
+        },
+        theme: MainTheme.build(),
+      ),
     );
   }
 }
